e7af7a1e03e17f8284160f279197d60be784317f
brcapitalone <brcapitalone@email.com>
2026-05-19 22:47:54 +0200

    fix sintassi send-email

--- api/send-email.js
+++ api/send-email.js
@@ -55,4 +55,7 @@ export default async function handler(req, res) {
     });
 
     res.status(200).json({ success: true });
-  } catch (err)
+} catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
