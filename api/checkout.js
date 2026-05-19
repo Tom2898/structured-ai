d7e9b609c832c7e8735de5d4e69446424d22875f
brcapitalone <brcapitalone@email.com>
2026-05-19 22:19:12 +0200

    fix checkout import ES module

--- api/checkout.js
+++ api/checkout.js
@@ -1,6 +1,6 @@
-const Stripe = require('stripe');
+import Stripe from 'stripe';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
@@ -20,4 +20,4 @@ module.exports = async function handler(req, res) {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-};
+}
